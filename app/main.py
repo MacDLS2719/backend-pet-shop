aab3dea9f34be79dcae329138188e493f2dc63e8
repositorioDahiana <brahiandiazladi@gmail.com>
2026-09-24 07:40:04 -0500

    acceso al forntend

--- app/main.py
+++ app/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
+        "https://pet-shop-production-8f5c.up.railway.app",
         "*",  # Permite peticiones desde cualquier origen (útil cuando despliegues el frontend en Vercel)
     ],
     allow_credentials=True,
